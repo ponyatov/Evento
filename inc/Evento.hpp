cb13d9f217df157bf19093aa54fef2a5efa1b2e7
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-03 13:02:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- inc/Evento.hpp
+++ inc/Evento.hpp
@@ -1,3 +1,8 @@
 #ifndef _EVENTO_H_
 #define _EVENTO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #endif  // _EVENTO_H_
--- lib/Evento.fs
+++ lib/Evento.fs
@@ -227,11 +227,29 @@ let hpp: unit =
         $"inc/{APP}.hpp",
         $"""#ifndef {H}
 #define {H}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
 #endif  // _EVENTO_H_
 """
     )
 
-let cpp: unit = File.WriteAllText($"src/{APP}.cpp", "")
+let cpp: unit =
+    File.WriteAllText($"src/{APP}.cpp", $"""#include "Evento.hpp"
+
+void arg(int argc, char *argv) {  //
+    fprintf(stderr, "arg[\%i] = <\%s>\n", argc, argv);
+}
+
+int main(int argc, char *argv[]) {  //
+    arg(0, argv[0]);
+    for (int i = 1; i < argc; i++) {  //
+        arg(i, argv[i]);
+    }
+}
+""")
 
 let lex: unit = File.WriteAllText($"src/{APP}.lex", "")
 
