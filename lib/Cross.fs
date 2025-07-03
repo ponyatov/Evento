d01b0198c5fbceea79d1e2232a7a7cee9a6d3437
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-03 13:27:24 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Cross.fs
+++ lib/Cross.fs
@@ -9,11 +9,15 @@ let f429disco: unit = //
     mkdir "hw/f429disco"
     mkdir "hw/f429disco/inc"
     mkdir "hw/f429disco/src"
+    touch "hw/f429disco/inc/f429disco.hpp"
+    touch "hw/f429disco/src/f429disco.cpp"
 
 let pillf103: unit = //
     mkdir "hw/pillf103"
     mkdir "hw/pillf103/inc"
     mkdir "hw/pillf103/src"
+    touch "hw/pillf103/inc/pillf103.hpp"
+    touch "hw/pillf103/src/pillf103.cpp"
 
 let hw: unit = //
     mkdir "hw"
