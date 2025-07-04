f748f5367d29e25923d7f969819a6880a4b7092f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-04 16:24:27 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod config;
+mod server;
 
 fn main() {
-    println!(
-        "Hello, world! @ http://{ip}:{port}",
-        ip = config::IP,
-        port = config::PORT
-    );
+    server::run();
 }
